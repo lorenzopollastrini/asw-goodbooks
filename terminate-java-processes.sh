3f597ea5bad93aa88e7ce9288ac24ff7085160f2
Lorenzo Pollastrini <88330570+lorenzopollastrini@users.noreply.github.com>
2022-12-27 15:15:22 +0100

    Passato da HSQLDB a PostgreSQL in connessioni
    Co-Authored-By: Valerio Parente <92206434+NinthLifeLeft66@users.noreply.github.com>
    Co-Authored-By: Federico D'Armini <63853572+fede-da@users.noreply.github.com>
    Co-Authored-By: ultra1998 <72657774+ultra1998@users.noreply.github.com>

--- terminate-java-processes.sh
+++ terminate-java-processes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo Halting all running java processes (except Eclipse)  
+echo Halting all running java processes except Eclipse  
 
 ps ax | awk '/java/ && !/eclipse/ {system("sudo kill "$1)}'
