ec0d0936b91b120f05ae2e8185c33a9789fef787
Lorenzo Pollastrini <88330570+lorenzopollastrini@users.noreply.github.com>
2023-01-28 18:22:21 +0100

    Aggiunta la possibilità di deployment con Kubernetes
    Co-Authored-By: Valerio Parente <92206434+NinthLifeLeft66@users.noreply.github.com>

--- do-init-connessioni.sh
+++ do-init-connessioni.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
+SERVICE_HOST=goodbooks
+INGRESS_PORT=31080
+
 # inizializza il db delle connessioni 
 
 # connessioni con autore 
 
-curl -X POST "http://localhost:8080/connessioni/connessioniautore" -H "accept: */*" -H "Content-Type: application/json" \
+curl -X POST ${SERVICE_HOST}/connessioni/connessioniautore --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT} -H "accept: */*" -H "Content-Type: application/json" \
      -d "{ \"utente\": \"Alice\", \"autore\": \"Tolstoj\"}"
 echo 
 
-curl -X POST "http://localhost:8080/connessioni/connessioniautore" -H "accept: */*" -H "Content-Type: application/json" \
+curl -X POST ${SERVICE_HOST}/connessioni/connessioniautore --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT} -H "accept: */*" -H "Content-Type: application/json" \
      -d "{ \"utente\": \"Alice\", \"autore\": \"Manzoni\"}"
 echo 
 
-curl -X POST "http://localhost:8080/connessioni/connessioniautore" -H "accept: */*" -H "Content-Type: application/json" \
+curl -X POST ${SERVICE_HOST}/connessioni/connessioniautore --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT} -H "accept: */*" -H "Content-Type: application/json" \
      -d "{ \"utente\": \"Bob\", \"autore\": \"Manzoni\"}"
 echo 
 
-curl -X POST "http://localhost:8080/connessioni/connessioniautore" -H "accept: */*" -H "Content-Type: application/json" \
+curl -X POST ${SERVICE_HOST}/connessioni/connessioniautore --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT} -H "accept: */*" -H "Content-Type: application/json" \
      -d "{ \"utente\": \"Bob\", \"autore\": \"Tolkien\"}"
 echo 
 
 # connessioni con recensore 
 
-curl -X POST "http://localhost:8080/connessioni/connessionirecensore" -H "accept: */*" -H "Content-Type: application/json" \
+curl -X POST ${SERVICE_HOST}/connessioni/connessionirecensore --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT} -H "accept: */*" -H "Content-Type: application/json" \
      -d "{ \"utente\": \"Bob\", \"recensore\": \"Woody\"}"
 echo 
 
-curl -X POST "http://localhost:8080/connessioni/connessionirecensore" -H "accept: */*" -H "Content-Type: application/json" \
+curl -X POST ${SERVICE_HOST}/connessioni/connessionirecensore --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT} -H "accept: */*" -H "Content-Type: application/json" \
      -d "{ \"utente\": \"Carlo\", \"recensore\": \"Roberto\"}"
 echo 
--- run-curl-client.sh
+++ run-curl-client.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 
+SERVICE_HOST=goodbooks
+INGRESS_PORT=31080
+
 # esegue un insieme di interrogazioni di esempio 
 
 echo "# tutte le recensioni"  
-echo $(curl -s localhost:8080/recensioni/recensioni)
+echo $(curl -s ${SERVICE_HOST}/recensioni/recensioni --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
 echo "# la recensione 1" 
-echo $(curl -s localhost:8080/recensioni/recensioni/1)
+echo $(curl -s ${SERVICE_HOST}/recensioni/recensioni/1 --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
 echo "# tutte le recensioni del recensore Woody" 
-echo $(curl -s localhost:8080/recensioni/cercarecensioni/recensore/Woody)
+echo $(curl -s ${SERVICE_HOST}/recensioni/cercarecensioni/recensore/Woody --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
 echo "# tutte le recensioni de I promessi sposi" 
-echo $(curl -s localhost:8080/recensioni/cercarecensioni/titolo/I%20promessi%20sposi)
+echo $(curl -s ${SERVICE_HOST}/recensioni/cercarecensioni/titolo/I%20promessi%20sposi --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
 echo "# tutte le recensioni dei libri di Tolstoj" 
-echo $(curl -s localhost:8080/recensioni/cercarecensioni/autore/Tolstoj)
+echo $(curl -s ${SERVICE_HOST}/recensioni/cercarecensioni/autore/Tolstoj --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
 echo "# tutte le connessioni" 
-echo $(curl -s localhost:8080/connessioni/connessioniautore)
+echo $(curl -s ${SERVICE_HOST}/connessioni/connessioniautore --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
-echo $(curl -s localhost:8080/connessioni/connessionirecensore)
+echo $(curl -s ${SERVICE_HOST}/connessioni/connessionirecensore --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
 echo "# tutte le connessioni di Alice" 
-echo $(curl -s localhost:8080/connessioni/connessioniautore/Alice)
+echo $(curl -s ${SERVICE_HOST}/connessioni/connessioniautore/Alice --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
-echo $(curl -s localhost:8080/connessioni/connessionirecensore/Alice)
+echo $(curl -s ${SERVICE_HOST}/connessioni/connessionirecensore/Alice --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
 echo "# le recensioni seguite da Alice" 
-echo $(curl -s localhost:8080/recensioni-seguite/recensioniseguite/Alice)
+echo $(curl -s ${SERVICE_HOST}/recensioni-seguite/recensioniseguite/Alice --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
 echo "# le recensioni seguite da Bob" 
-echo $(curl -s localhost:8080/recensioni-seguite/recensioniseguite/Bob)
+echo $(curl -s ${SERVICE_HOST}/recensioni-seguite/recensioniseguite/Bob --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
 echo "# le recensioni seguite da Carlo" 
-echo $(curl -s localhost:8080/recensioni-seguite/recensioniseguite/Carlo)
+echo $(curl -s ${SERVICE_HOST}/recensioni-seguite/recensioniseguite/Carlo --connect-to ${SERVICE_HOST}:80:kube-cluster:${INGRESS_PORT})
 echo 
 
